6286311c9fa63d5bbcbf970befc48880878b86eb
Michele <m.ippolito@outlook.com>
2014-12-14 11:12:47 +0100

    Version 1

---
+++ ScienceFunding/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: KSPAssembly("ScienceFunding", 1, 0)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ScienceFunding")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pota Studios")]
+[assembly: AssemblyProduct("ScienceFunding")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51baf5d2-8d3b-4163-8152-f426c296b8a1")]
---
+++ ScienceFunding/ScienceFunding.cs
@@ -0,0 +1,106 @@
+﻿using KSP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Reflection;
+using System.IO;
+
+namespace ScienceFunding
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class ScienceFunding : MonoBehaviour
+    {
+        public float fundsMult = 1000;
+        public float repMult = 1;
+
+        public void Awake()
+        {
+            LoadConfiguration();
+            GameEvents.OnScienceRecieved.Add(ScienceReceivedHandler);
+            ScienceFunding.Log("listening for science...");
+        }
+
+        /// <summary>
+        /// Science transmission handler: computes the funds and reputation boni
+        /// and awards them to the player.
+        /// </summary>
+        public void ScienceReceivedHandler(float science, ScienceSubject sub)
+        {
+            string msg = "Received " + science + " science points";
+            ScienceFunding.Log(msg);
+
+            float funds = science * this.fundsMult;
+            float rep = science * this.repMult;
+
+            if (Funding.Instance != null)
+            {
+                Funding.Instance.AddFunds(funds, TransactionReasons.ScienceTransmission);
+                ScienceFunding.Log("Added " + funds + " funds");
+            }
+
+            if (Reputation.Instance != null)
+            {
+                Reputation.Instance.AddReputation(rep, TransactionReasons.ScienceTransmission);
+                ScienceFunding.Log("Added " + rep + " reputation");
+            }
+
+            string msgContent = "Your research efforts in the field of " + sub.title + " have granted you " +
+                                funds + " additional funds and increased your reputation by " + rep;
+
+            PostMessage("More funds available!",
+                        msgContent,
+                        MessageSystemButton.MessageButtonColor.GREEN,
+                        MessageSystemButton.ButtonIcons.COMPLETE); 
+        }
+
+        public void OnDestroy()
+        {
+            GameEvents.OnScienceRecieved.Remove(ScienceReceivedHandler);
+            ScienceFunding.Log("OnDestroy, removing handler.");
+        }
+
+        #region Utilities
+
+        public void LoadConfiguration()
+        {
+            ConfigNode node = GetConfig();
+            this.fundsMult = float.Parse(node.GetValue("funds"));
+            this.repMult = float.Parse(node.GetValue("rep"));
+        }
+
+        static ConfigNode GetConfig()
+        {
+            string assemblyPath = Path.GetDirectoryName(typeof(ScienceFunding).Assembly.Location);
+            string filePath = Path.Combine(assemblyPath, "settings.cfg");
+
+            ScienceFunding.Log("Loading settings file:" + filePath);
+
+            ConfigNode result = ConfigNode.Load(filePath).GetNode("SCIENCE_FUNDING_SETTINGS");
+            ScienceFunding.Log(result.ToString());
+
+            return result;
+        }
+
+        static void PostMessage(string title,
+                                string message,
+                                MessageSystemButton.MessageButtonColor messageButtonColor,
+                                MessageSystemButton.ButtonIcons buttonIcons)
+        {
+            MessageSystem.Message msg = new MessageSystem.Message(
+                    title,
+                    message,
+                    messageButtonColor,
+                    buttonIcons);
+            MessageSystem.Instance.AddMessage(msg);
+        }
+
+        static void Log(string msg)
+        {
+            Debug.Log("[ScienceFunding]: " + msg);
+        }   
+
+        #endregion      
+    }
+}
